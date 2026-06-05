fe6f81271f9246fc18deeb27fb11ac5198ef7fa2
mike3213 <limxyz117248@163.com>
2026-06-05 18:21:19 +0800

    26-06-05_coding

--- 26-06-03/1220A.cpp
+++ 26-06-03/1220A.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
 int main(void){
-	
+	int numOfn = 0, numOfz = 0;
+	char in = '\0';
+	while(cin >> in){
+		if(in == 'n')
+			numOfn++;
+		else if(in == 'z')
+			numOfz++;
+	}
+	/*
+	long long sum = 0;
+	for(int i = 0; i < numOfn + numOfz; ++i){
+		if(i < numOfz)
+			sum = sum * 2 + 0;
+		else
+			sum = sum * 2 + 1;
+	}
+	*/
+	while(numOfn-- > 0)
+		cout << '1' << ' ';
+	while(numOfz-- > 0){
+		cout << '0';
+		if(numOfz != 0)
+			cout << ' ';
+	}
+
 	return 0;
 }
