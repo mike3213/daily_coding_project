0a42a37a9c1a8902e646cd79d6598d08d0c5ccef
mike3213 <limxyz117248@163.com>
2026-08-15 23:56:57 +0800

    26-08-15

---
+++ 26-08-15/1354B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main() {
+    int cases = 0;
+    cin >> cases;
+    while(cases-- > 0) {
+        string str;
+        cin >> str;
+        
+
+        if(cases != 0)
+            cout << '\n';
+    }
+
+    return 0;
+}
