3f04f1603d9a42e3351cffdb26d5d13ff1fc6711
mike3213 <limxyz117248@163.com>
2026-09-18 23:54:27 +0800

    26-09-18:

---
+++ 26-09-18/1927D.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int cases = 0;
+    cin >> cases;
+    while(cases-- > 0) {
+        int n = 0;
+        cin >> n;
+        vector<int> num(n, 0);
+        for(auto &x : num)
+            cin >> x;
+        vector<pair<int, int>> interval;
+        int last = 0;
+        for(int i = 1; i < n; ++i) {
+            while(i < n && num[i] == num[last]) {
+                i++;
+            }
+            int len = (i - 1 - last + 1);
+            if(len >= 2) {
+                interval.emplace_back(last, last + len - 1);
+            }
+            last = i;
+        }
+
+        int q = 0;
+        cin >> q;
+        while(q-- > 0) {
+            int a = 0, b = 0;
+            cin >> a >> b;
+            if(interval.size() == 0) {
+                cout << a << ' ' << a + 1;
+            } else {
+                int left = 0, right = interval.size(), mid = 0;
+                bool flag = false;
+                while(left < right) {
+                    mid = left + (right - left) / 2;
+                    if(a > interval[mid].first + 1) {
+                        left = mid + 1;
+                    } else if(a < interval[mid].first + 1) {
+                        right = mid;
+                    } else {
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag && (left == 0 || left == n)) {
+                    cout << a << ' ' << b;
+                } else {
+                    bool ans = true;
+                    int L = 0, R = 0;
+                    if(flag) {
+                        if(b <= interval[mid].second + 1) {
+                            ans = false;
+                        } else {
+                            L = a, R = interval[mid].second + 1 + 1;
+                        }
+                    } else {
+                        if(b <= interval[left - 1].second + 1) {
+                            ans = false;
+                        } else {
+                            L = a, R = interval[left - 1].second + 1 + 1;
+                        }
+                    }
+                    if(ans) {
+                        cout << L << ' ' << R;
+                    } else 
+                        cout << -1 << ' ' << -1;
+                }
+   
+            }
+        
+            cout << '\n';
+        }
+
+        if(cases > 0)
+            cout << '\n';
+    }
+
+
+    return 0;
+}
