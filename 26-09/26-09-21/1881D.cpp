0d6345209ec0024c62d911113ac9100c77d74b2f
mike3213 <limxyz117248@163.com>
2026-09-22 00:30:21 +0800

    26-09-21

--- 26-09/26-09-21/1881D.cpp
+++ 26-09/26-09-21/1881D.cpp
@@ -1,22 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long get_final_num(long long num, long long n) {
-    if(num < 0)
-        return -1;
-    long long left = 0, right = num + 1;
-    while(left < right) {
-        long long mid = left + (right - left) / 2;
-        if(pow(mid, n) < num) {
-            left = mid + 1;
-        } else if(pow(mid, n) > num) {
-            right = mid;
-        } else {
-            return mid;
-        }
-    }
-    return -1;
-}
-
 int main() {
     int cases = 0;
     cin >> cases;
@@ -24,24 +7,34 @@ int main() {
         int n = 0;
         cin >> n;
         vector<int> num(n, 0);
-        unsigned long long mult = 1;
-        for(auto &x : num) {
+        for(auto &x : num) 
             cin >> x;
-            mult = mult * x;
+        unordered_map<int, int> cnt;
+        for(int i = 0; i < n; ++i) {
+            int t = num[i];
+            for(int j = 2; j * j <= t; ++j) {
+                while(t % j == 0) {
+                    cnt[j]++;
+                    t /= j;
+                }
+            }
+            if(t > 1) {
+                cnt[t]++;
+            }
         }
-        long long p = get_final_num(mult, n);
-        if(p == -1)
-            cout << "NO";
-        else {
+        bool flag = true;
+        for(auto &p : cnt) {
+            if(p.second % n != 0) {
+                cout << "NO";
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
             cout << "YES";
-        }
-
         if(cases > 0)
             cout << '\n';
     }    
-
-
-
 
     return 0;
 }
